882652b30d9cc706f7e948d53f7fc794bd1fbd82
Elvis Nuñez <elvisnunez@me.com>
2018-05-26 11:53:45 +0200

    Initial Commit

---
+++ TheNewApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheNewApp
+//
+//  Created by Elvis Nuñez on 26/05/2018.
+//  Copyright © 2018 The New Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
